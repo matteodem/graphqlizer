240ca4c0042e1ab3301d411830010a81aeacf089
Matteo De Micheli <matteodem@protonmail.com>
2018-02-23 13:13:00 +0100

    Add resolver.create tests

--- tests/ResolverApiTest.js
+++ tests/ResolverApiTest.js
@@ -3,12 +3,55 @@ import {
   resolver,
 } from '../lib'
 import {
-  defaultCrud,
-  fieldsConfig,
-  inputSimpleSchema,
-  fakeCollectionForResolvers,
-} from './testData'
+  customSchema,
+  fakeCollection,
+} from './ResolverApiTestData'
 
 Tinytest.add('Graphqlizer - Public API - resolver', function (test) {
-  //test.equal(resolver.create('Alien', ))
+  test.equal(
+    resolver.create('Book', fakeCollection)(
+      null,
+      { 
+        data: { 
+          age: 21,
+          awesomeName: 'matt',
+        },
+      },
+    ), 
+    ['createBook', { age: 21, awesomeName: 'matt' }],
+  )
+  
+  test.equal(
+    resolver.create('Book', fakeCollection, customSchema)(
+       null,
+      { 
+        data: { 
+          age: 21,
+          awesomeName: 'matt',
+          secondAwesomeName: 'mattaya',
+          isAwesome: true,
+        },
+      },
+    ), 
+    ['createBook', { 
+      age: 21,
+      awesomeName: 'matt',
+      secondAwesomeName: 'mattaya',
+      isAwesome: true,
+    }],
+  )
+  
+  try {
+    resolver.create('Book', fakeCollection, customSchema)(
+       null,
+      { 
+        data: { 
+          age: 21,
+          awesomeName: 'matt',
+        },
+      },
+    )
+    test.fail()
+  } catch (e) {
+  }
 })
